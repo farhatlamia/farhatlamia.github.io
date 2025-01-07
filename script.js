deed11fc391cf125b28dbf7b0815c3ace38a5517
farhatlamia <flamia.borsha@gmail.com>
2025-01-07 14:04:07 -0600

    Updated styles in style.css

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
-// Add to script.js
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
+document.querySelectorAll('nav a').forEach(link => {
+    link.addEventListener('click', function (e) {
         e.preventDefault();
         document.querySelector(this.getAttribute('href')).scrollIntoView({
             behavior: 'smooth'
